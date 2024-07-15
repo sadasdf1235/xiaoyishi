f79ae85933f64b0757d1d61ce0cc363b550eeb5a
liuding <116145942+sadasdf1235@users.noreply.github.com>
2024-07-15 22:34:26 +0800

    exhibition effect
    效果图展示

---
+++ README.md
@@ -0,0 +1,11 @@
+# 校易市
+
+### 效果展示
+
+1. <img src="C:\Users\ld\Pictures\校易市\Image_1720656942036.png" style="zoom:50%;" />
+2. <img src="C:\Users\ld\Pictures\校易市\Image_1720657018316.png" style="zoom:50%;" />
+3. <img src="C:\Users\ld\Pictures\校易市\Image_1720656943895.png" style="zoom:50%;" />
+4. <img src="C:\Users\ld\Pictures\校易市\Image_1720656945567.png" style="zoom:50%;" />
+5. <img src="C:\Users\ld\Pictures\校易市\Image_1720656947190.png" style="zoom:50%;" />
+6. <img src="C:\Users\ld\Pictures\校易市\Image_1720657013744.png" style="zoom:50%;" />
+7. <img src="C:\Users\ld\Pictures\校易市\Image_1720657016410.png" style="zoom:50%;" />
--- lib/controllers/message_controller.dart
+++ lib/controllers/message_controller.dart
@@ -4,20 +4,20 @@ import '../routes/app_routes.dart';
 class MessageController extends GetxController{
 
   List messageList = [
-    // {
-    //   "avatar" : 'https://ts1.cn.mm.bing.net/th?id=OIP-C._YFRagbOM8FbGUSUJy-m6QAAAA&w=250&h=250&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2',
-    //   "userName" : 'zzz_y',
-    //   "message" : '可以小刀吗？',
-    //   "lastMessageTime" : '04/12 09:32',
-    //   "unreadNum" : '2',
-    // },
-    // {
-    //   "avatar" : 'https://ts2.cn.mm.bing.net/th?id=OIP-C.ebDwpQ8AJ8ykQbZ-2VDoUwAAAA&w=250&h=250&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2',
-    //   "userName" : '至幸',
-    //   "message" : '产品细节怎么样',
-    //   "lastMessageTime" : '04/13 11:52',
-    //   "unreadNum" : '1',
-    // },
+    {
+      "avatar" : 'https://ts1.cn.mm.bing.net/th?id=OIP-C._YFRagbOM8FbGUSUJy-m6QAAAA&w=250&h=250&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2',
+      "userName" : 'zzz_y',
+      "message" : '可以小刀吗？',
+      "lastMessageTime" : '04/12 09:32',
+      "unreadNum" : '2',
+    },
+    {
+      "avatar" : 'https://ts2.cn.mm.bing.net/th?id=OIP-C.ebDwpQ8AJ8ykQbZ-2VDoUwAAAA&w=250&h=250&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2',
+      "userName" : '至幸',
+      "message" : '产品细节怎么样',
+      "lastMessageTime" : '04/13 11:52',
+      "unreadNum" : '1',
+    },
   ];
 
   // 是否展示删除图标
