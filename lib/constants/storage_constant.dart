57209d6a9fa4aa001deba33b5af7200b99bb6f44
liuding <116145942+sadasdf1235@users.noreply.github.com>
2024-10-04 12:03:30 +0800

    feat: update

--- lib/constants/storage_constant.dart
+++ lib/constants/storage_constant.dart
@@ -1,5 +1,4 @@
-class Constants {
-  static const String BASE_URL = "http://192.168.50.46:8080";
+class StorageConstant {
   static const String TOKEN = "token";
   static const String USER_INFO = "user_info";
 }
